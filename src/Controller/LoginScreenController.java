13bd0c772a02d356da8b12b26bb966f0ef80ef5a
User <User@192.168.25.187>
2017-01-19 15:19:02 -0200

    Start the project..

---
+++ src/Controller/LoginScreenController.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gabriel Mello de Oliveira
+ */
+
+public class LoginScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/Controller/MainScreenController.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author User
+ */
+public class MainScreenController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
